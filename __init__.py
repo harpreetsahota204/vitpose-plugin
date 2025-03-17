f2dcbc3ce98a07b1e45385a57a9b5a70a0acb9c7
Harpreet Sahota <harpreetsahota204@gmail.com>
2025-03-17 14:51:40 -0500

    remove skeleton drawing

--- __init__.py
+++ __init__.py
@@ -155,29 +155,29 @@ class ViTPoseEstimator(foo.Operator):
             confidence_threshold = confidence_threshold
             )
         
-        keypoint_labels = [
-            "nose", "left_eye", "right_eye", "left_ear", "right_ear",
-            "left_shoulder", "right_shoulder", "left_elbow", "right_elbow",
-            "left_wrist", "right_wrist", "left_hip", "right_hip",
-            "left_knee", "right_knee", "left_ankle", "right_ankle"
-        ]
+        # to do: find a better way to define the skeletons for partial skeletons
+        # keypoint_labels = [
+        #     "nose", "left_eye", "right_eye", "left_ear", "right_ear",
+        #     "left_shoulder", "right_shoulder", "left_elbow", "right_elbow",
+        #     "left_wrist", "right_wrist", "left_hip", "right_hip",
+        #     "left_knee", "right_knee", "left_ankle", "right_ankle"
+        # ]
         
-        # Define skeleton edges
-        edges = [
-            [0, 1], [0, 2], [1, 3], [2, 4],  # Face
-            [5, 7], [7, 9], [6, 8], [8, 10],  # Arms
-            [5, 6], [5, 11], [6, 12], [11, 12],  # Torso
-            [11, 13], [13, 15], [12, 14], [14, 16]  # Legs
-        ]
+        # # Define skeleton edges
+        # edges = [
+        #     [0, 1], [0, 2], [1, 3], [2, 4],  # Face
+        #     [5, 7], [7, 9], [6, 8], [8, 10],  # Arms
+        #     [5, 6], [5, 11], [6, 12], [11, 12],  # Torso
+        #     [11, 13], [13, 15], [12, 14], [14, 16]  # Legs
+        # ]
 
-        skeleton = fo.KeypointSkeleton(labels=keypoint_labels, edges=edges)
+        # skeleton = fo.KeypointSkeleton(labels=keypoint_labels, edges=edges)
 
-        view.skeletons = {
-            output_field: skeleton,
-            }
+        # view.skeletons = {
+        #     output_field: skeleton,
+        #     }
 
 
-        
         ctx.ops.reload_dataset()
 
     def __call__(
